2f4b76f82ce9636efce9d19579a41277a9e11c2d
Pal Marci <20556689+palmarci@users.noreply.github.com>
2023-07-10 14:40:47 +0200

    Update query.py

--- query.py
+++ query.py
@@ -80,9 +80,9 @@ def main():
 		print(f'reusing hardcoded token: {masterToken}')
 	
   	callMvaApi(masterToken, '/customerAPI/v1/personalInformation')
-#	ids = ['2003692668', '9161015985413772348', '9161015972513693591', '9161015993013737567']
+#	ids = ['', '', '', '']
 	#for i in ids:
-	#	callMvaApi(masterToken, f'/customerAPI/v1/customerAccount/{i}/subscription')
+	#	callMvaApi(masterToken, f'/customerAPI/v1/customerAccount/{i}/subscription') not working
 	callMvaApi(masterToken, '/customerAPI/v1/billingAccount')
 
 
